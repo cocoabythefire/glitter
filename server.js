4d81567f288db344169c1d2c59c8b6d23f58dd42
Brittany Tarvin <bntarvin@fadingred.com>
2015-03-30 14:05:39 -0700

    split the server (for testing)

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var app = require('./app');
 
 var server = app.listen(process.env.PORT || 3000, function () {
 
