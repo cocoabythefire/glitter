be3b58329fa914d9ea2db3a81b7b49d6ffa44224
Brittany Tarvin <bntarvin@fadingred.com>
2015-08-07 18:27:06 -0700

    real response for root.

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.set('db', db);
 app.use(bodyParser.json()); // for parsing application/json
 
 app.get('/', function (req, res) {
-  res.send('Hello Whiiiiiiiiy Blair!');
+  res.send('This is the glitter service!');
 });
 
 // Authentication Middleware
